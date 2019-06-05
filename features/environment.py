b617d3f5537fef0a1e6edb8187d96912e045e551
petrisorionel <petrisor.ionel@gmail.com>
2019-06-05 15:40:30 +0300

    setUp, tearDown in before and after scenario
    It is possible to do this in before and after scenario?

--- features/environment.py
+++ features/environment.py
@@ -3,10 +3,18 @@ from browser import Browser
 from pages.home_page import HomePage
 from pages.search_results_page import SearchResultsPage
 
-def before_all(context):
+def before_scenario(context, scenario):
     context.browser = Browser()
     context.home_page = HomePage()
     context.search_results_page = SearchResultsPage()
 
-def after_all(context):
+# def before_all(context):
+#     context.browser = Browser()
+#     context.home_page = HomePage()
+#     context.search_results_page = SearchResultsPage()
+
+def after_scenario(context, scenario):
     context.browser.close()
+
+# def after_all(context):
+#     context.browser.close()
